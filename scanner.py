3adc94ae7e07255a7cf64f157718c39153d1800a
Farkhat Yakubov <farhatyakubov@gmail.com>
2020-08-22 21:45:06 +0600

    Added REST API get-request pattern

--- scanner.py
+++ scanner.py
@@ -3,12 +3,23 @@ import json
 
 
 async def handle(request):
-    response_obj = {'status': 'success'}
+    ip = request.match_info.get('ip')
+    begin_port = request.match_info.get('begin_port')
+    end_port = request.match_info.get('end_port')
+    response_obj = \
+        {'data':
+            {
+                'ip': ip,
+                'begin_port': begin_port,
+                'end_port': end_port
+            }
+        }
+
     return web.Response(text=json.dumps(response_obj), status=200)
 
 
 app = web.Application()
-app.router.add_get('/scan', handle)
+app.router.add_get('/scan/{ip}/{begin_port}/{end_port}', handle)
 
 if __name__ == '__main__':
     web.run_app(app)
