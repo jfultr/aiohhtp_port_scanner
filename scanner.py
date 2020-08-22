1b3974f3a649c0768d7cb6c2983f06e21d410ec3
Farkhat Yakubov <farhatyakubov@gmail.com>
2020-08-23 03:51:37 +0600

    Added port scanning functions;

--- scanner.py
+++ scanner.py
@@ -1,11 +1,40 @@
 from aiohttp import web
 import json
+import asyncio
+
+
+def catch_exception(func):
+    async def decorated_function(ip, port, scan_loop):
+        try:
+            await func(ip, port, scan_loop)
+            return port, True
+        except asyncio.exceptions.TimeoutError:
+            return port, False
+    return decorated_function
+
+
+@catch_exception
+async def check_port(ip, port, scan_loop):
+    conn = asyncio.open_connection(ip, port, loop=scan_loop)
+    await asyncio.wait_for(conn, timeout=3)
+
+
+async def run(ip, begin_port, end_port, scan_loop):
+    tasks = [asyncio.ensure_future(check_port(ip, p, scan_loop)) for p in range(begin_port, end_port)]
+    responses = await asyncio.gather(*tasks)
+    return responses
 
 
 async def handle(request):
     ip = request.match_info.get('ip')
-    begin_port = request.match_info.get('begin_port')
-    end_port = request.match_info.get('end_port')
+    begin_port = int(request.match_info.get('begin_port'))
+    end_port = int(request.match_info.get('end_port'))
+
+    loop = asyncio.get_event_loop()
+    future = await asyncio.ensure_future(run(ip, begin_port, end_port, loop))
+
+    print(future)
+
     response_obj = \
         {'data':
             {
